e20eaddee9ceaa42375b0934c1e323372d7c542f
Philippe Marschall <philippe.marschall@gmail.com>
2022-04-03 00:24:11 +0200

    test valve

---
+++ spring4shell-valve/src/test/java/com/github/marschall/spring4shell/valve/OkServlet.java
@@ -0,0 +1,18 @@
+package com.github.marschall.spring4shell.valve;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OkServlet extends HttpServlet {
+  
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    response.setStatus(HttpServletResponse.SC_OK);
+  }
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    response.setStatus(HttpServletResponse.SC_OK);
+  }
+
+}
---
+++ spring4shell-valve/src/test/java/com/github/marschall/spring4shell/valve/SimpleFilterValueTests.java
@@ -0,0 +1,81 @@
+package com.github.marschall.spring4shell.valve;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.junit.jupiter.api.Test;
+
+class SimpleFilterValueTests {
+  
+  @Test
+  void invoke() throws LifecycleException, IOException {
+    Tomcat tomcat = new Tomcat();
+
+    Connector connector = new Connector(Http11NioProtocol.class.getName());
+    connector.setPort(8080);
+    connector.setProperty("address", "127.0.0.1");
+    connector.setThrowOnFailure(true);
+    tomcat.getService().addConnector(connector);
+    tomcat.setConnector(connector);
+    
+    tomcat.getHost().getPipeline().addValve(new SimpleFilterValue());
+
+    Context context = tomcat.addContext("", Paths.get("src", "test", "resources", "docroot").toAbsolutePath().toString());
+    String servletName = "ok-servlet";
+    Tomcat.addServlet(context, servletName, new OkServlet());
+    context.addServletMappingDecoded("/", servletName);
+
+    tomcat.start();
+
+    try {
+      sentNotBlockedRequest();
+      sentBlockedRequest();
+    } finally {
+      tomcat.stop();
+      tomcat.destroy();
+    }
+  }
+
+  private void sentNotBlockedRequest() throws IOException {
+    int statusCode = sendBody("module.resources.context.parent.pipeline.first.suffix=.j%25%7Bc2%7Di");
+    assertEquals(HttpServletResponse.SC_OK, statusCode, "HTTP status code");
+  }
+
+  private void sentBlockedRequest() throws IOException {
+    int statusCode = sendBody("class.module.classLoader.resources.context.parent.pipeline.first.suffix=.j%25%7Bc2%7Di");
+    assertEquals(HttpServletResponse.SC_FORBIDDEN, statusCode, "HTTP status code");
+  }
+
+  private int sendBody(String body) throws IOException {
+    URL url = new URL("http://127.0.0.1:8080/");
+    byte[] bytes = body.getBytes(StandardCharsets.UTF_8);
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    connection.setRequestMethod("POST");
+    connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+    connection.setRequestProperty("Content-Length", Integer.toString(bytes.length));
+    connection.setDoOutput(true);
+    connection.connect();
+    try {
+      try (OutputStream outputStream = connection.getOutputStream()) {
+        outputStream.write(bytes);
+      }
+      return connection.getResponseCode();
+    } finally {
+      connection.disconnect();
+    }
+  }
+
+}
